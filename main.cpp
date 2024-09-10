53c0c011090fa7af16381b9ee96a2a2e37e0d7e9
Fabio Tardivo <ftardivo@nmsu.edu>
2024-09-10 13:21:58 -0600

    Print timing summary

--- main.cpp
+++ main.cpp
@@ -94,7 +94,7 @@ int main(int argc, char * argv[])
             FznStatisticsHelper::printStatistics(stats, solver, fzn_model, search, cout);
         }
 
-        Timer::summary();
+        std::cout << Timer::summary();
 
         exit(EXIT_SUCCESS);
     }
