58e26f0dffe872b814c0c84a127edf746c5e56f8
Fabio Tardivo <tardivo.fabio@gmail.com>
2026-02-07 18:21:27 -0500

    [ML] Stuff

--- fzn_search_helper.h
+++ fzn_search_helper.h
@@ -148,7 +148,7 @@ std::function<Branches(void)> FznSearchHelper::getMLSearchStrategy(Fzn::Model co
                     {
                         nBoundedVars += array_int_var[varIdx]->isBound();
                     }
-                    if (nBoundedVars >= 0)
+                    if (nBoundedVars % 2 == 0)
                     {
                         return search_strategy();
                     }
