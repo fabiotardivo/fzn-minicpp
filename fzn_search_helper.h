60c62a147d8cb345c466b423f85a532798e480a1
Fabio Tardivo <tardivo.fabio@gmail.com>
2026-02-07 19:12:40 -0500

    [ML] Stuff

--- fzn_search_helper.h
+++ fzn_search_helper.h
@@ -129,7 +129,7 @@ std::function<Branches(void)> FznSearchHelper::getMLSearchStrategy(Fzn::Model co
                         nAssigned += var->isBound();
                     }
 
-                    if (nAssigned < 5)
+                    if (nAssigned < nVars / 5)
                     {
                         int_var_t bestVar = nullptr;
                         auto bestVal = std::numeric_limits<int>::max();;
